a1e9a535065fdcef2ecd2fae8facc347a51d8e6e
trzytematyczna <monikrakoczy@gmail.com>
2015-06-05 18:44:01 +0200

    init 2

--- src/homomorphic_sense/Main.java
+++ src/homomorphic_sense/Main.java
@@ -1,5 +1,37 @@
 package homomorphic_sense;
 
+import org.ejml.simple.SimpleMatrix;
+
+import ij.*;
+import ij.io.Opener;
+import ij.plugin.TextReader;
+import ij.process.ImageProcessor;
+
 public class Main {
 
+	public static void main(String[] args) {
+		
+		TextReader textReader = new TextReader();
+		
+		ImageProcessor mriIp = textReader.open("res/MR_noisy.csv");
+		ImageProcessor snrIp = textReader.open("res/MR_SNR.csv");
+		
+		double[][] mriD = new double[mriIp.getHeight()][mriIp.getWidth()];
+		double[][] snrD = new double[snrIp.getHeight()][snrIp.getWidth()];
+		for (int i = 0; i < mriIp.getHeight(); i++){
+			for (int j = 0; j < mriIp.getWidth(); j++) {
+				mriD[i][j] = mriIp.getPixelValue(i, j);
+				snrD[i][j] = snrIp.getPixelValue(i, j);
+			}
+		}
+		
+		SimpleMatrix mri = new SimpleMatrix(mriD);
+		SimpleMatrix snr = new SimpleMatrix(snrD);
+		
+		SimpleMatrix gaussian = RiceHomomorfEst.rice_hommomorf_est(mri, snr, 4.8, 2, RiceHomomorfEst.GAUSSIAN);
+		SimpleMatrix rician = RiceHomomorfEst.rice_hommomorf_est(mri, snr, 4.8, 2, RiceHomomorfEst.RICIAN);
+		
+
+	}
+	
 }
---
+++ src/homomorphic_sense/RiceHomomorfEst.java
@@ -0,0 +1,24 @@
+package homomorphic_sense;
+
+import org.ejml.simple.SimpleMatrix;
+
+import ij.process.ImageProcessor;
+
+public class RiceHomomorfEst {
+
+	static int GAUSSIAN = 1;
+	static int RICIAN = 2;
+	
+	
+	public static SimpleMatrix rice_hommomorf_est(SimpleMatrix In, SimpleMatrix SNR, double LPF, int Modo, int noiseType) {
+		
+		
+		return null;
+		
+	}
+	
+	public static SimpleMatrix em_ml_rice2D(SimpleMatrix In, int N, SimpleMatrix Ws) {
+		return null;
+	}
+	
+}
