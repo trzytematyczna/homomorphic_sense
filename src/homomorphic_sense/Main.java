bd94a251238bdb8213fae81eecf9827cf3951aad
trzytematyczna <monikrakoczy@gmail.com>
2015-06-08 15:27:09 +0200

    funckje do macierzy

--- src/homomorphic_sense/Main.java
+++ src/homomorphic_sense/Main.java
@@ -1,35 +1,47 @@
-package homomorphic_sense_opencv;
+package homomorphic_sense;
 
+import ij.ImagePlus;
 import ij.plugin.TextReader;
+import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.imgcodecs.*;
-import org.opencv.videoio.*;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.loadLibrary("opencv_java300");
+//		System.loadLibrary("opencv_java300");
 		
 		TextReader textReader = new TextReader();
 		ImageProcessor mriIp = textReader.open("res/MR_noisy.csv");
 		ImageProcessor snrIp = textReader.open("res/MR_SNR.csv");
+		System.out.println(mriIp.getPixelValue(0, 0));
+		System.out.println(mriIp.getPixelValue(1, 1));
+		mriIp.add(4);
+		System.out.println(mriIp.getPixelValue(0, 0));
+		System.out.println(mriIp.getPixelValue(1, 1));
+		ImagePlus asd = new ImagePlus("ASD", mriIp);
+	    float[] jedne = {2,1,1,1,1,1};
+	    float[] dwa = {1,1,1,1,1,1};
+	    float[][] www = {jedne,dwa};
+	    float[][] ppp = new float[7][8];
+//	    System.out.println(www[3][0]);
+	   ImageProcessor asdasd = new FloatProcessor(ppp);
+	   System.out.println(asdasd.getPixelValue(0, 0));
+	   System.out.println(asdasd.getHeight());
+	   System.out.println(asdasd.getWidth());
+//		Mat mri = new Mat(mriIp.getHeight(), mriIp.getWidth(), CvType.CV_64FC1);
+//		Mat snr = new Mat(snrIp.getHeight(), snrIp.getWidth(), CvType.CV_64FC1);
+//		for (int i = 0; i < mriIp.getHeight(); i++){
+//			for (int j = 0; j < mriIp.getWidth(); j++) {
+//				mri.put(i, j, mriIp.getPixelValue(i, j));
+//				snr.put(i, j, snrIp.getPixelValue(i, j));
+//			}
+//		}
 		
-		Mat mri = new Mat(mriIp.getHeight(), mriIp.getWidth(), CvType.CV_64FC1);
-		Mat snr = new Mat(snrIp.getHeight(), snrIp.getWidth(), CvType.CV_64FC1);
-		for (int i = 0; i < mriIp.getHeight(); i++){
-			for (int j = 0; j < mriIp.getWidth(); j++) {
-				mri.put(i, j, mriIp.getPixelValue(i, j));
-				snr.put(i, j, snrIp.getPixelValue(i, j));
-			}
-		}
-		
-		Mat ricianMap =  RiceHommomorfEst.rice_hommomorf_est(mri, snr, 4.8, 2, RiceHommomorfEst.RICIAN, 3);
-		
-		Imgcodecs.imwrite("MR_Rician_map.csv", ricianMap);
+//		Mat ricianMap =  RiceHommomorfEst.rice_hommomorf_est(mri, snr, 4.8, 2, RiceHommomorfEst.RICIAN, 3);
+//		
+//		Imgcodecs.imwrite("MR_Rician_map.csv", ricianMap);
 		
 	}
 }
